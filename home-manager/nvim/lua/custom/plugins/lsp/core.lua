7540c4c9a80f3e60ea18efc2b908a0db7a40bf65
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2024-11-09 01:18:31 -0800

    neovim: Update core lsp config

--- home-manager/nvim/lua/custom/plugins/lsp/core.lua
+++ home-manager/nvim/lua/custom/plugins/lsp/core.lua
@@ -1,42 +1,54 @@
 local M = {}
 
 local set_keymaps = function(bufnr)
-  local function nmap(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
-
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  local map = function(keys, func, desc, mode)
+    mode = mode or "n"
+    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
   end
 
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  -- Jump to the definition of the word under your cursor.
+  --  This is where a variable was first declared, or where a function is defined, etc.
+  --  To jump back, press <C-t>.
+  map("gd", require("telescope.builtin").lsp_definitions, "Go to definition")
 
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-  nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  -- Find references for the word under your cursor.
+  map("gr", require("telescope.builtin").lsp_references, "Goto references")
+
+  -- Jump to the implementation of the word under your cursor.
+  --  Useful when your language has ways of declaring types without an actual implementation.
+  map("gI", require("telescope.builtin").lsp_implementations, "Go to implementation")
+
+  -- Jump to the type of the word under your cursor.
+  --  Useful when you're not sure what type a variable is and you want to see
+  --  the definition of its *type*, not where it was *defined*.
+  map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type definition")
+
+  -- Fuzzy find all the symbols in your current document.
+  --  Symbols are things like variables, functions, types, etc.
+  map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "Document symbols")
+
+  -- Fuzzy find all the symbols in your current workspace.
+  --  Similar to document symbols, except searches over your entire project.
+  map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
+
+  -- Rename the variable under your cursor.
+  --  Most Language Servers support renaming across files, etc.
+  map("<leader>rn", vim.lsp.buf.rename, "Rename")
+
+  -- Execute a code action, usually your cursor needs to be on top of an error
+  -- or a suggestion from your LSP for this to activate.
+  map("<leader>ca", vim.lsp.buf.code_action, "Code action", { "n", "x" })
+
+  -- WARN: This is not Goto Definition, this is Goto Declaration.
+  --  For example, in C this would take you to the header.
+  map("gD", vim.lsp.buf.declaration, "Go to declaration")
 
   -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-  -- Lesser used LSP functionality
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
+  map("K", vim.lsp.buf.hover, "Hover Documentation")
+  map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 end
 
 local augroup_format = vim.api.nvim_create_augroup("custom-lsp-format", { clear = true })
-
-local function clear_autocmds(group, buffer)
-  vim.api.nvim_clear_autocmds({ group = group, buffer = buffer })
-end
 
 local function autocmd(event, group, callback, buffer, opts)
   opts = opts or {}
@@ -50,8 +62,34 @@ local function autocmd(event, group, callback, buffer, opts)
   })
 end
 
-local set_autocommands = function(_, buffer)
-  clear_autocmds(augroup_format, buffer)
+local set_autocommands = function(client, buffer)
+  -- The following two autocommands are used to highlight references of the
+  -- word under your cursor when your cursor rests there for a little while.
+  --    See `:help CursorHold` for information about when this is executed
+  --
+  -- When you move your cursor, the highlights will be cleared (the second autocommand).
+  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+    local highlight_augroup = vim.api.nvim_create_augroup("custom-lsp-highlight", { clear = false })
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      buffer = buffer,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.document_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      buffer = buffer,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.clear_references,
+    })
+
+    vim.api.nvim_create_autocmd("LspDetach", {
+      group = vim.api.nvim_create_augroup("custom-lsp-detach", { clear = true }),
+      callback = function(event2)
+        vim.lsp.buf.clear_references()
+        vim.api.nvim_clear_autocmds({ group = "custom-lsp-highlight", buffer = event2.buf })
+      end,
+    })
+  end
 
   autocmd("BufWritePre", augroup_format, function()
     vim.lsp.buf.format({ async = false })
@@ -59,8 +97,6 @@ local set_autocommands = function(_, buffer)
 end
 
 M.on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
   set_keymaps(bufnr)
   set_autocommands(client, bufnr)
 end
@@ -68,7 +104,11 @@ end
 M.make_config = function(config)
   return vim.tbl_deep_extend("force", {
     on_attach = M.on_attach,
-    capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = vim.tbl_deep_extend(
+      "force",
+      vim.lsp.protocol.make_client_capabilities(),
+      require("cmp_nvim_lsp").default_capabilities()
+    ),
   }, config or {})
 end
 
